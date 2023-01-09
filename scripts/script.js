e87de07139bf35d5db6d5c18c02edc2b1c4f1c25
JakFranceschini <jakson.franceschini@gmail.com>
2023-01-09 20:27:08 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let cor1 = document.getElementById('cor1')
+let cor2 = document.getElementById('cor2')
+let cor3 = document.getElementById('cor3')
+
+cor1.addEventListener('click', function() {
+    document.getElementById ('tenisPrincipal').src = '../img/iconeTenis1.png'
+})
+
+cor2.addEventListener('click', function() {
+    document.getElementById ('tenisPrincipal').src = '../img/iconeTenis2.png'
+})
+
+cor3.addEventListener('click', function() {
+    document.getElementById ('tenisPrincipal').src = '../img/iconeTenis3.png'
+})
